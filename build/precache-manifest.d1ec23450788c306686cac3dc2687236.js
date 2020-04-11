3c285b7646e63cb159e5ef2eaed41480b8a2cceb
WilsonChen40172 <kelvin811215@hotmail.com>
2020-04-11 17:46:56 +0800

    first commit

---
+++ build/precache-manifest.d1ec23450788c306686cac3dc2687236.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661b6d237ff7e6801e096ae3eb73fa07",
+    "url": "/WilsonChen40172/reactBuild20200411/index.html"
+  },
+  {
+    "revision": "826f2925d25a074dd75e",
+    "url": "/WilsonChen40172/reactBuild20200411/static/css/main.4e2eb8fe.chunk.css"
+  },
+  {
+    "revision": "5b48a0254b18d19fa989",
+    "url": "/WilsonChen40172/reactBuild20200411/static/js/2.2277bd71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WilsonChen40172/reactBuild20200411/static/js/2.2277bd71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "826f2925d25a074dd75e",
+    "url": "/WilsonChen40172/reactBuild20200411/static/js/main.886473f6.chunk.js"
+  },
+  {
+    "revision": "84521485a700ce24c6de",
+    "url": "/WilsonChen40172/reactBuild20200411/static/js/runtime-main.2e18cbec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WilsonChen40172/reactBuild20200411/static/media/logo.5d5d9eef.svg"
+  }
+]);
